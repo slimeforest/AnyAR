c0156d06943c71293b72b72d858b77707cc59f17
jack battle <jackonno78@hotmail.com>
2022-03-18 02:09:24 -0500

    removed unnecessary code

--- AnyAR/Models/UserItemCellCollectionViewCell.swift
+++ AnyAR/Models/UserItemCellCollectionViewCell.swift
@@ -12,22 +12,7 @@ class UserItemCellCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!    
     @IBOutlet weak var nodeThumbnailPrev: UIImageView!
-    
-    override var isSelected: Bool {
-        didSet {
-            if self.isSelected {
-                self.layer.borderColor = UIColor.systemBlue.cgColor
-                self.layer.borderWidth = 2.0
-                self.layer.cornerRadius = 10
-                print("isSelected \(self.isSelected)")
-            }else {
-                self.layer.borderColor = UIColor.clear.cgColor
-                self.layer.borderWidth = 0
-                print("isSelectedNot \(self.isSelected)")
-            }
-        }
-    }
-    
+
     func config(_ item: Item) {
         nameLabel.text = item.itemName
         nodeThumbnailPrev.image = item.itemImage
